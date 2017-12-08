0d7d3ebf84ad8ab85307f457c195f9c04c85ec37
brosenbusch <8brosenbusch@stpatrick.org>
2017-12-08 14:42:45 -0600

    Add files via upload

---
+++ EAN.js
@@ -0,0 +1,79 @@
+const Random = require('./Random.js');
+
+const EAN = function(){
+
+    let sysCode;//3 digits
+    let manCode;//5 digits
+    let proCode;//4 digits
+    let checkDigit = check();
+
+
+    function makeSysCode(){
+        let r = discreteRangeIn(1,8);
+        if (r = 1){
+            return "040";//Germany
+        }
+        else if (r = 2){
+            return "070";//Norway
+        }
+        else if (r = 3){
+            return "590";//Poland
+        }
+        else if (r = 4){
+            return "003";//USA
+        }
+        else if (r = 5){
+            return "690"//China
+        }
+        else if (r = 6){
+            return "539";//Ireland
+        }
+        else if (r = 7){
+            return "";
+        }
+        else if (r = 8){
+
+        }
+
+
+    }
+    function makeManCode(){
+        return Random().digit(5);
+    }
+    function makeProCode(){
+        return Random().digit(4);
+    }
+    function check(){
+        //create this function
+        let code = sysCode+manCode+proCode;
+        let sum = 0;
+        for(let c=0;c<12;c++){
+            if(c%2 ==0){
+                sum += 1*code[c];
+            }
+            else{
+                sum += 3*code[c];
+            }
+        }
+        console.log("the sum is ="+sum);
+        let remainder = sum %10;
+        if(remainder == 0){
+            return 0;
+        }
+        return 10-remainder;
+    }
+
+    function read(){
+        //create this function
+
+    }
+    return {sysCode,manCode,proCode,checkDigit,read};
+};
+
+function test(){
+    let e = new EAN();
+    console.log(e.checkDigit);
+
+}
+//test();
+module.exports = EAN;
